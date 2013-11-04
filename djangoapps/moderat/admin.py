3b14b36f4279a1fe11a827501673d8f5f426747c
adn <eapv_25@hotmail.com>
2013-11-04 03:11:45 -0500

    cruds of new tables for lastq

--- djangoapps/moderat/admin.py
+++ djangoapps/moderat/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin 
-from djangoapps.moderat.models import Quest, Choice
+from djangoapps.moderat.models import Quest, Choice ,Lastquest, LastChoice
 	#nuestra app.
 
 admin.site.register(Quest)#registrar clase en el sistema de admin.
 admin.site.register(Choice)
+admin.site.register(Lastquest)
+admin.site.register(LastChoice)
